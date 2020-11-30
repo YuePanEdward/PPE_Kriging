502bd652c5099ad3165c5921f083aea3f3eeb378
needhelp-proj <65340575+needhelp-proj@users.noreply.github.com>
2020-11-30 18:07:38 +0100

    Add files via upload

---
+++ kriging_ppe.m
@@ -0,0 +1,9 @@
+function[Field_values, Field_variances, conversion_factor] = kriging_ppe(data,Field_values_init)
+%% Function for the kriging
+% Input: data (3,n_data) = x-, y- and h value of the points, Field_values_init (n,m) =
+% matrix with the size that the output needs to have
+% Output: Field_values (n,m) = the data value or blup, Field_variances
+% (n,m) = the estimated error variances, conversion_factor (DX,DY) =
+% conversion factors between Matrix and real world length
+
+end
